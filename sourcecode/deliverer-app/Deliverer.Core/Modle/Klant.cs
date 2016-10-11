17608273b002270fad39d89ee328ace80f7529ac
RobbeDelie <robbedelie@hotmail.com>
2016-10-11 15:55:56 +0200

    aanmaken core (del app)
    code schrijven voor het aanleveren van data. voorlopig hardcoded data

---
+++ sourcecode/deliverer-app/Deliverer.Core/Modle/Klant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverer.Core.Modle
+{
+    public class Klant
+    {
+        public string Naam
+        {
+            get;set;
+        }
+        public string Locatie
+        {
+            get;set;
+        }
+    }
+}
---
+++ sourcecode/deliverer-app/Deliverer.Core/Repository/KlantRepository.cs
@@ -0,0 +1,31 @@
+﻿using Deliverer.Core.Modle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverer.Core.Repository
+{
+    public class KlantRepository
+    {
+        private static List<Klant> klanten = new List<Klant>()
+        {
+            new Klant()
+            {
+                Naam="Robbe",
+                Locatie="51.228643, 4.415705"
+            },
+            new Klant()
+            {
+                Naam="Jef",
+                Locatie="51.229772, 4.413956"
+            },
+            new Klant()
+            {
+                Naam="Pim",
+                Locatie="51.229137, 4.413205"
+            }
+        };
+    }
+}
